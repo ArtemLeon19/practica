ac0ad0ba6414f7ea254c8177f2e5026b50496169
ArtemLeon19 <124984131+ArtemLeon19@users.noreply.github.com>
2023-03-31 12:11:49 +0300

    lab6

---
+++ lab6/DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess.Interfaces;
+using Domain.SQL;
+
+namespace DataAccess.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, Domain.Interfaces.IUserRepository
+    {
+        public UserRepository(PracticaContext repositoryContext) : base(repositoryContext)
+        {
+        }
+
+    }
+}
